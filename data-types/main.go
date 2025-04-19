55d76e04e85010225c03fe35e7433d1fc2fb249a
juancamilosalas <juansalasprogrammer@gmail.com>
2025-04-19 15:51:08 -0500

    data types

---
+++ data-types/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variables
+	// var name type = value
+	var name string = "Juan"
+	// var name = value
+	var lastName = "Salas"
+	// name := value
+	secondName := "Salas"
+	fmt.Println("Hello, my name is", name, lastName, secondName)
+
+	// Numbers
+	var currentYear int16 = 2025
+	var asoYear int8 = 25
+	age := 27
+
+	fmt.Println("Current year:", currentYear, asoYear, age)
+}
